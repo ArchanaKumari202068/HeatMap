b120072d631db814bc94d31f8efe8c1f01b99b68
Archana bhardwaj <ArchanaKumari202068@gmail.com>
2022-08-28 15:47:50 +0530

    file updated

---
+++ fol.py
@@ -0,0 +1,12 @@
+import pandas as pd
+df = pd.read_csv('train.csv')
+data = pd.read_csv('train.csv ')
+import folium
+from folium.plugins import HeatMap
+stationArr = df[['Y','X']].values
+m = folium.Map(location=[stationArr[0][0],stationArr[0][1]], zoom_start=12)
+HeatMap(stationArr[:50000], radius=15).add_to(m)
+# m.add_child(folium.ClickForMarker(popup="hiiii") )
+m.save('index.html')
+
+# m = folium.Map(location=[45.5236, -122.6750])
